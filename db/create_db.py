a8c74674b7aa82f7c97ee9077ae7f208f3388c98
Ferranti <support@planetcrypto.com>
2014-08-11 14:27:02 -0500

    Fixed FEC data size discrepencies in ALL tables

--- db/create_db.py
+++ db/create_db.py
@@ -4,124 +4,124 @@ import psycopg2
 #http://www.fec.gov/finance/disclosure/metadata/DataDictionaryCandidateMaster.shtml
 candidate_master_sql = """CREATE TABLE candidate_master ( \
                                            ID SERIAL NOT NULL, \
-                                           CAND_ID VARCHAR(9) NOT NULL, \
-                                           CAND_NAME VARCHAR(200), \
-                                           CAND_PTY_AFFILIATION VARCHAR(3), \
+                                           CAND_ID VARCHAR(14) NOT NULL, \
+                                           CAND_NAME VARCHAR(250), \
+                                           CAND_PTY_AFFILIATION VARCHAR(14), \
                                            CAND_ELECTION_YR SMALLINT, \
-                                           CAND_OFFICE_ST VARCHAR(2), \
+                                           CAND_OFFICE_ST VARCHAR(4), \
                                            CAND_OFFICE VARCHAR(1), \
-                                           CAND_OFFICE_DISTRICT VARCHAR(2), \
-                                           CAND_ICI VARCHAR(1), \
-                                           CAND_STATUS VARCHAR(1), \
-                                           CAND_PCC VARCHAR(9), \
-                                           CAND_ST1 VARCHAR(34), \
-                                           CAND_ST2 VARCHAR(34), \
-                                           CAND_CITY VARCHAR(30), \
-                                           CAND_ST VARCHAR(2), \
-                                           CAND_ZIP VARCHAR(9));"""
+                                           CAND_OFFICE_DISTRICT VARCHAR(4), \
+                                           CAND_ICI VARCHAR(4), \
+                                           CAND_STATUS VARCHAR(4), \
+                                           CAND_PCC VARCHAR(14), \
+                                           CAND_ST1 VARCHAR(40), \
+                                           CAND_ST2 VARCHAR(40), \
+                                           CAND_CITY VARCHAR(35), \
+                                           CAND_ST VARCHAR(5), \
+                                           CAND_ZIP VARCHAR(14));"""
 
 #http://www.fec.gov/finance/disclosure/metadata/DataDictionaryCommitteeMaster.shtml
 commitee_master_sql = """CREATE TABLE committee_master ( \
                                            ID SERIAL NOT NULL, \
-                                           CMTE_ID VARCHAR(9) NOT NULL,\
-                                           CMTE_NM VARCHAR(200), \
-                                           TRES_NM VARCHAR(90), \
-                                           CMTE_ST1 VARCHAR(34), \
-                                           CMTE_ST2 VARCHAR(34), \
-                                           CMTE_CITY VARCHAR(30), \
-                                           CMTE_ST VARCHAR(2), \
-                                           CMTE_ZIP VARCHAR(9), \
-                                           CMTE_DSGN VARCHAR(1), \
-                                           CMTE_TP VARCHAR(1), \
-                                           CMTE_PTY_AFFILIATION VARCHAR(3), \
-                                           CMTE_FILING_FREQ VARCHAR(1), \
-                                           ORG_TP VARCHAR(1), \
-                                           CONNECTED_ORG_NM VARCHAR(200), \
-                                           CAND_ID VARCHAR(9));"""
+                                           CMTE_ID VARCHAR(14) NOT NULL,\
+                                           CMTE_NM VARCHAR(250), \
+                                           TRES_NM VARCHAR(100), \
+                                           CMTE_ST1 VARCHAR(40), \
+                                           CMTE_ST2 VARCHAR(40), \
+                                           CMTE_CITY VARCHAR(35), \
+                                           CMTE_ST VARCHAR(4), \
+                                           CMTE_ZIP VARCHAR(14), \
+                                           CMTE_DSGN VARCHAR(4), \
+                                           CMTE_TP VARCHAR(4), \
+                                           CMTE_PTY_AFFILIATION VARCHAR(5), \
+                                           CMTE_FILING_FREQ VARCHAR(4), \
+                                           ORG_TP VARCHAR(4), \
+                                           CONNECTED_ORG_NM VARCHAR(250), \
+                                           CAND_ID VARCHAR(14));"""
 
 #http://www.fec.gov/finance/disclosure/metadata/DataDictionaryCandCmteLinkage.shtml                        
 candidate_linkage_sql = """CREATE TABLE candidate_linkage ( \
                                            ID SERIAL NOT NULL, \
-                                           CAND_ID VARCHAR(9) NOT NULL, \
+                                           CAND_ID VARCHAR(14) NOT NULL, \
                                            CAND_ELECTION_YR SMALLINT, \
                                            FEC_ELECTION_YR SMALLINT, \
-                                           CMTE_ID VARCHAR(9), \
-                                           CMTE_TP VARCHAR(1), \
-                                           CMTE_DSGN VARCHAR(1), \
+                                           CMTE_ID VARCHAR(14), \
+                                           CMTE_TP VARCHAR(4), \
+                                           CMTE_DSGN VARCHAR(4), \
                                            LINKAGE_ID BIGINT);"""
                                            
 #http://www.fec.gov/finance/disclosure/metadata/DataDictionaryCommitteetoCommittee.shtml
 comm_to_comm_sql = """CREATE TABLE comm_to_comm ( \
                                         ID SERIAL NOT NULL, \
-                                        CMTE_ID VARCHAR(9), \
-                                        AMNDT_IND VARCHAR(1), \
-                                        RPT_TP VARCHAR(3), \
-                                        TRANSACTION_PGI VARCHAR(5), \
-                                        IMAGE_NUM VARCHAR(11), \
-                                        TRANSACTION_TP VARCHAR(3), \
-                                        ENTITY_TP VARCHAR(3), \
-                                        NAME VARCHAR(200), \
-                                        CITY VARCHAR(30), \
-                                        STATE VARCHAR(2), \
-                                        ZIP_CODE VARCHAR(9), \
+                                        CMTE_ID VARCHAR(14), \
+                                        AMNDT_IND VARCHAR(4), \
+                                        RPT_TP VARCHAR(5), \
+                                        TRANSACTION_PGI VARCHAR(10), \
+                                        IMAGE_NUM VARCHAR(15), \
+                                        TRANSACTION_TP VARCHAR(5), \
+                                        ENTITY_TP VARCHAR(5), \
+                                        NAME VARCHAR(250), \
+                                        CITY VARCHAR(35), \
+                                        STATE VARCHAR(5), \
+                                        ZIP_CODE VARCHAR(14), \
                                         EMPLOYER VARCHAR(50), \
                                         OCCUPATION VARCHAR(50), \
                                         TRANSACTION_DT DATE, \
                                         TRANSACTION_AMT INTEGER, \
-                                        OTHER_ID VARCHAR(9), \
-                                        TRAN_ID VARCHAR(32), \
-                                        FILE_NUM VARCHAR(22), \
-                                        MEMO_CD VARCHAR(1), \
+                                        OTHER_ID VARCHAR(14), \
+                                        TRAN_ID VARCHAR(35), \
+                                        FILE_NUM VARCHAR(25), \
+                                        MEMO_CD VARCHAR(5), \
                                         MEMO_TEXT VARCHAR(100), \
                                         SUB_ID BIGINT UNIQUE NOT NULL);"""
                                         
 # http://www.fec.gov/finance/disclosure/metadata/DataDictionaryContributionstoCandidates.shtml
 cand_to_comm_sql = """CREATE TABLE cand_to_comm ( \
                                       ID SERIAL NOT NULL, \
-                                      CMTE_ID VARCHAR(9) NOT NULL, \
-                                      AMNDT_IND VARCHAR(1), \
-                                      RPT_TP VARCHAR(3), \
-                                      TRANSACTION_PGI VARCHAR(5), \
-                                      IMAGE_NUM VARCHAR(11), \
-                                      TRANSACTION_TP VARCHAR(3), \
-                                      ENTITY_TP VARCHAR(3), \
-                                      NAME VARCHAR(200), \
-                                      CITY VARCHAR(30), \
-                                      STATE VARCHAR(2), \
-                                      ZIP_CODE VARCHAR(9), \
+                                      CMTE_ID VARCHAR(14) NOT NULL, \
+                                      AMNDT_IND VARCHAR(5), \
+                                      RPT_TP VARCHAR(5), \
+                                      TRANSACTION_PGI VARCHAR(10), \
+                                      IMAGE_NUM VARCHAR(15), \
+                                      TRANSACTION_TP VARCHAR(5), \
+                                      ENTITY_TP VARCHAR(5), \
+                                      NAME VARCHAR(250), \
+                                      CITY VARCHAR(35), \
+                                      STATE VARCHAR(5), \
+                                      ZIP_CODE VARCHAR(14), \
                                       EMPLOYER VARCHAR(50), \
                                       OCCUPATION VARCHAR(50), \
                                       TRANSACTION_DT DATE, \
                                       TRANSACTION_AMT INTEGER, \
-                                      OTHER_ID VARCHAR(9), \
-                                      CAND_ID VARCHAR(9), \
-                                      TRAN_ID VARCHAR(32), \
-                                      FILE_NUM VARCHAR(22), \
-                                      MEMO_CD VARCHAR(1), \
+                                      OTHER_ID VARCHAR(14), \
+                                      CAND_ID VARCHAR(14), \
+                                      TRAN_ID VARCHAR(35), \
+                                      FILE_NUM VARCHAR(25), \
+                                      MEMO_CD VARCHAR(5), \
                                       MEMO_TEXT VARCHAR(100), \
                                       SUB_ID BIGINT UNIQUE NOT NULL);"""
 
 #http://www.fec.gov/finance/disclosure/metadata/DataDictionaryContributionsbyIndividuals.shtml
 individual_contrib_sql = """CREATE TABLE indiv_contrib ( \
-                                          CMTE_ID VARCHAR(9) NOT NULL, \
-                                          AMNDT_IND VARCHAR(1), \
-                                          RPT_TP VARCHAR(3), \
-                                          TRANSACTION_PGI VARCHAR(5), \
-                                          IMAGE_NUM VARCHAR(11), \
-                                          TRANSACTION_TP VARCHAR(3), \
-                                          ENTITY_TP VARCHAR(3), \
-                                          NAME VARCHAR(200), \
-                                          CITY VARCHAR(30), \
-                                          STATE VARCHAR(2), \
-                                          ZIP_CODE VARCHAR(9), \
+                                          CMTE_ID VARCHAR(14) NOT NULL, \
+                                          AMNDT_IND VARCHAR(5), \
+                                          RPT_TP VARCHAR(5), \
+                                          TRANSACTION_PGI VARCHAR(9), \
+                                          IMAGE_NUM VARCHAR(15), \
+                                          TRANSACTION_TP VARCHAR(5), \
+                                          ENTITY_TP VARCHAR(5), \
+                                          NAME VARCHAR(250), \
+                                          CITY VARCHAR(35), \
+                                          STATE VARCHAR(5), \
+                                          ZIP_CODE VARCHAR(14), \
                                           EMPLOYER VARCHAR(50), \
                                           OCCUPATION VARCHAR(50), \
                                           TRANSACTION_DT DATE, \
                                           TRANSACTION_AMT INTEGER, \
-                                          OTHER_ID VARCHAR(9), \
-                                          TRAN_ID VARCHAR(32), \
-                                          FILE_NUM VARCHAR(22), \
-                                          MEMO_CD VARCHAR(1), \
+                                          OTHER_ID VARCHAR(14), \
+                                          TRAN_ID VARCHAR(35), \
+                                          FILE_NUM VARCHAR(25), \
+                                          MEMO_CD VARCHAR(5), \
                                           MEMO_TEXT VARCHAR(100), \
                                           SUB_ID BIGINT UNIQUE NOT NULL);"""
 for i in range(2004, 2015, 2):
