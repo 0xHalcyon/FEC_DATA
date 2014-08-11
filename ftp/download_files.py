0f72696d91f1d512aeeabdceeb2730c92c4f362e
Ferranti <support@planetcrypto.com>
2014-08-11 15:55:06 -0500

    Modified .gitignore

--- ftp/download_files.py
+++ ftp/download_files.py
@@ -8,21 +8,21 @@ ftp =FTP("ftp.fec.gov")
 
 ftp.login()
 
-files = {"/FEC/%s/cm%s.zip"    : "../data/%s/cm%s.zip",   # Committee Master File
-         "/FEC/%s/cn%s.zip"    : "../data/%s/cn%s.zip",   # Candidate Master File
-	 "/FEC/%s/ccl%s.zip"   : "../data/%s/cn%s.zip",   # Candidate Committee Linkage File
-	 "/FEC/%s/oth%s.zip"   : "../data/%s/oth%s.zip",  # Any transaction from one committee to another
-	 "/FEC/%s/pas2%s.zip"  : "../data/%s/pas2%s.zip", # Contributions to candidates (and other expenditures) from committees
-	 "/FEC/%s/indiv%s.zip" : "../data/%s/indiv%s.zip" # Contributions by individuals
+files = {"/FEC/%s/cm%s.zip"    : "data/%s/cm%s.zip",   # Committee Master File
+         "/FEC/%s/cn%s.zip"    : "data/%s/cn%s.zip",   # Candidate Master File
+	 "/FEC/%s/ccl%s.zip"   : "data/%s/cn%s.zip",   # Candidate Committee Linkage File
+	 "/FEC/%s/oth%s.zip"   : "data/%s/oth%s.zip",  # Any transaction from one committee to another
+	 "/FEC/%s/pas2%s.zip"  : "data/%s/pas2%s.zip", # Contributions to candidates (and other expenditures) from committees
+	 "/FEC/%s/indiv%s.zip" : "data/%s/indiv%s.zip" # Contributions by individuals
 	}
 for year in range(config.start_year, 2015, 2):
   year_suffix = str(year)[2:]
   for f in sorted(files):
-    if not os.path.isdir("../data/%s" % year):
-      os.mkdir("../data/%s"  % year)
+    if not os.path.isdir("data/%s" % year):
+      os.mkdir("data/%s"  % year)
     to_get = f % (year, year_suffix)
     save_to = files[f] % (year, year_suffix)
-    print "Downloading: %s" % to_get
+    print "Downloading: %s%s" % (url, to_get)
     ftp.retrbinary("RETR %s" % to_get, open(save_to, "wb").write)
     print "Saving to: %s" % save_to
     print "Done"
