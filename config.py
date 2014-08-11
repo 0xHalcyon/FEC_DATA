36b52891d40eeb356fff584081e656863b5820f8
Ferranti <support@planetcrypto.com>
2014-08-11 14:36:35 -0500

    Add ftp directory and config.py file

---
+++ config.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+###################################
+## MODIFY THIS FILE TO YOUR PREFERENCES ##
+##################################
+
+# Start year to maintain data from (must be >= 1998) default is 2004
+
+start_year = 2004
+
+# Database user, default is postgres
+
+db_user = "postgres"
+
+# Database password, default is none
+
+db_password = ""
+
+# DB Prefix, default = FEC_
+
+db_prefix = "FEC_"
+
