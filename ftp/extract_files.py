b7fc4c6ced0e15ba6e92eb765950e5beacec2bd5
Ferranti <support@planetcrypto.com>
2014-08-11 21:34:41 -0500

    Added extract functions

--- ftp/extract_files.py
+++ ftp/extract_files.py
@@ -5,7 +5,7 @@ import config
 
 files = {"data/%s/cm%s.zip"    : "data/%s/cm%s/",    # Committee Master File
          "data/%s/cn%s.zip"    : "data/%s/cn%s/",    # Candidate Master File
-	 "data/%s/ccl%s.zip"    : "data/%s/ccl%s/",    # Candidate Committee Linkage File
+	 "data/%s/ccl%s.zip"   : "data/%s/ccl%s/",    # Candidate Committee Linkage File
 	 "data/%s/oth%s.zip"   : "data/%s/oth%s/",   # Any transaction from one committee to another
 	 "data/%s/pas2%s.zip"  : "data/%s/pas2%s/",  # Contributions to candidates (and other expenditures) from committees
 	 "data/%s/indiv%s.zip" : "data/%s/indiv%s/", # Contributions by individuals
@@ -21,15 +21,16 @@ files_1998 = {"data/%s/cm%s.zip"    : "data/%s/cm%s/",    # Committee Master Fil
 for year in range(config.start_year, config.end_year, 2):
   year_suffix = str(year)[2:]
   
-  if year == 1998:
+  if year <= 1998:
     
-    for f in files:
+    for f in files_1998:
       archive = f % (year, year_suffix)
-      extract_to = files[f] % (year, year_suffix)
+      extract_to = files_1998[f] % (year, year_suffix)
       if not os.path.isdir(extract_to):
         os.mkdir(extract_to)
       zf = zipfile.ZipFile(archive)
       print "Extracting %s to %s" % (archive, extract_to)
+      zf.extractall(extract_to)
       
   else: 
     
@@ -40,4 +41,5 @@ for year in range(config.start_year, config.end_year, 2):
         os.mkdir(extract_to)
       zf = zipfile.ZipFile(archive)
       print "Extracting %s to %s" % (archive, extract_to)
+      zf.extractall(extract_to)
     
