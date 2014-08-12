9846b9d21b35956f9c5ed7075e04f7a9692353e9
Ferranti <support@planetcrypto.com>
2014-08-12 00:58:25 -0500

    Restored config file to default settings

--- config.py
+++ config.py
@@ -4,15 +4,15 @@
 ## MODIFY THIS FILE TO YOUR PREFERENCES ##
 ##########################################
 
-# Start year to maintain data from (must be >= 1998) default is 2004
+# Start year to maintain data from (must be >= 1998) default is 1998
 # Can be 1998, 2000, 2002, 2004, 2006, 2008, 2010, 2012, 2014 ..
 
-start_year = 2000
+start_year = 1998
 
 # End year to maintain data to, must be >= start_year+1, default is 2015
 # can be 1999, 2001, 2003, 2005, 2007, 2009, 2011, 2013, 2015 ...
 
-end_year = 2003
+end_year = 2015
 
 # Database user, default is postgres
 
