f3f47b36c854053f1a41f44a793829368fc148f0
Ferranti <support@planetcrypto.com>
2014-08-12 16:28:07 -0500

    Fixed sqlalchemy connect issue

--- db/create_db.py
+++ db/create_db.py
@@ -154,6 +154,7 @@ for year in range(config.start_year, config.end_year, 2):
     eng_conn.execute(create_db_stmt)
     eng_conn.connection.connection.set_isolation_level(1)
     eng_conn.close()
+    engine.close()
     sleep(1)
     conn = psycopg2.connect(dbname=config.db_prefix.lower()+str(year),
 			  user=config.db_user,
