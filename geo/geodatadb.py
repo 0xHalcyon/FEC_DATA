b73505d7329bc3e4775cc88e9febc9cb917230d2
Ferranti <support@planetcrypto.com>
2014-08-13 01:29:32 -0500

    Fixed derp indent errors

--- geo/geodatadb.py
+++ geo/geodatadb.py
@@ -34,45 +34,45 @@ def gengeodb(cwd, db_prefix, db_user, db_password, db_host, db_port):
     except sqlalchemy.exc.ProgrammingError as e:
       print "Error:%s\nDid you modify your configuration file and run make createuser?" % e
       os.sys.exit(1)
-    conn = psycopg2.connect(dbname=db_prefix.lower()+"geozipcodes",
-	  	            user=db_user,
-			    password=db_password,
-			    host=db_host,
-			    port=db_port
-			    )
-    cur = conn.cursor()
+  conn = psycopg2.connect(dbname=db_prefix.lower()+"geozipcodes",
+ 	                  user=db_user,
+			  password=db_password,
+			  host=db_host,
+			  port=db_port
+			  )
+  cur = conn.cursor()
     
-    try:
-      cur.execute("CREATE TABLE zipcodes ( \
-	           zip VARCHAR(10) UNIQUE NOT NULL, \
-	           type TEXT, \
-	           primary_city TEXT, \
-	           acceptable_cities TEXT, \
-	           unacceptable_cities TEXT, \
-	           state VARCHAR(5), \
-	           county TEXT, \
-	           timezone TEXT, \
-	           area_codes TEXT, \
-	           latitude REAL, \
-	           logitude REAL, \
-	           world_region TEXT, \
-	           country TEXT, \
-	           decommissioned TEXT, \
-	           estimated_population TEXT, \
-	           notes TEXT")
-    except psycopg2.Error as e:
-      print "We could not create the table, %s" % e
-      os.sys.exit(1)
-    for zipcode in zipcodes:
+  try:
+    cur.execute("CREATE TABLE zipcodes ( \
+                 zip VARCHAR(10) UNIQUE NOT NULL, \
+	         type TEXT, \
+	         primary_city TEXT, \
+	         acceptable_cities TEXT, \
+	         unacceptable_cities TEXT, \
+	         state VARCHAR(5), \
+	         county TEXT, \
+	         timezone TEXT, \
+	         area_codes TEXT, \
+	         latitude REAL, \
+	         logitude REAL, \
+	         world_region TEXT, \
+	         country TEXT, \
+	         decommissioned TEXT, \
+	         estimated_population TEXT, \
+	         notes TEXT")
+  except psycopg2.Error as e:
+    print "We could not create the table, %s" % e
+    os.sys.exit(1)
+  for zipcode in zipcodes:
 	
-      stmt = """INSERT INTO foo (zip, type, primary_city, acceptable_cities, unacceptable_cities,\ 
-                                 state, county, timezone, area_codes, latitude, longitude, \
-                                 world_region, country, decommissioned, estimated_population, notes) \
-                                 VALUES (%s, %s, %s, %s, %s, %s,\
-                                 %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""" % \
-                                 tuple(zipcode)
-      print stmt
-      cur.execute(stmt)
-    conn.commit()
-    cur.close()
-    conn.close()
+    stmt = """INSERT INTO foo (zip, type, primary_city, acceptable_cities, unacceptable_cities,\ 
+                               state, county, timezone, area_codes, latitude, longitude, \
+                               world_region, country, decommissioned, estimated_population, notes) \
+                               VALUES (%s, %s, %s, %s, %s, %s,\
+                               %s, %s, %s, %s, %s, %s, %s, %s, %s, %s);""" % \
+                               tuple(zipcode)
+    print stmt
+    cur.execute(stmt)
+  conn.commit()
+  cur.close()
+  conn.close()
