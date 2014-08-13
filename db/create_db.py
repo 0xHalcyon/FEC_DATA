d879800e3af11cce614b3cb8baaa2b83068db098
Ferranti <support@planetcrypto.com>
2014-08-12 20:42:45 -0500

    Added pandas as prerequisite

--- db/create_db.py
+++ db/create_db.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import psycopg2
 import os
+import sqlalchemy
 from sqlalchemy import create_engine
 from time import sleep
 
@@ -140,25 +141,30 @@ def create_db(start_year, end_year, cwd, db_prefix, db_user, db_password, db_hos
       cur = conn.cursor()
     
     except psycopg2.OperationalError as e:
-      print "Database %s%s does not exist yet, creating now" % (db_prefix.lower(), year)
-      engine_stmt = 'postgresql+psycopg2://%s:%s@%s:%s/template1' % \
+      try:
+        print "Database %s%s does not exist yet, creating now" % (db_prefix.lower(), year)
+        engine_stmt = 'postgresql+psycopg2://%s:%s@%s:%s/template1' % \
 	              (db_user, db_password, db_host, db_port)
-      engine = create_engine(engine_stmt)
-      eng_conn = engine.connect()
-      eng_conn.connection.connection.set_isolation_level(0)
-      create_db_stmt = "CREATE DATABASE %s%s" % (db_prefix.lower(), year)
-      eng_conn.execute(create_db_stmt)
-      eng_conn.connection.connection.set_isolation_level(1)
-      eng_conn.close()
-      engine.dispose()
-      sleep(1)
+        engine = create_engine(engine_stmt)
+        eng_conn = engine.connect()
+        eng_conn.connection.connection.set_isolation_level(0)
+        create_db_stmt = "CREATE DATABASE %s%s" % (db_prefix.lower(), year)
+        eng_conn.execute(create_db_stmt)
+        eng_conn.connection.connection.set_isolation_level(1)
+        eng_conn.close()
+        engine.dispose()
+        sleep(1)
+      except sqlalchemy.exc.ProgrammingError as e:
+	print "Error:%s\nDid you modify your configuration file and run make createuser?" % e
+	os.sys.exit(1)
       conn = psycopg2.connect(dbname=db_prefix.lower()+str(year),
-			      user=db_user,
+	  	              user=db_user,
 			      password=db_password,
 			      host=db_host,
 			      port=db_port
-			     )
+			      )
       cur = conn.cursor()
+      
     
   
     if year <= 1998:
