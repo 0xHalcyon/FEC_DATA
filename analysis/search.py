be616937b5491d28cf4b8082b67419a7a39f79a5
Ferranti <support@planetcrypto.com>
2014-08-13 00:14:06 -0500

    Added geo location file and beginnings of data analysis tool

---
+++ analysis/search.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import pandas
+import sys
+from sqlalchemy import create_engine
+
+
+def search_names(field, query, year, db_prefix, db_user, db_password, db_host, db_port):
+  engine_stmt = 'postgresql+psycopg2://%s:%s@%s:%s/%s%s' % \
+                (db_user, db_password, db_host, db_port, db_prefix, str(year))
+  engine = create_engine(engine_stmt)
+  if year <= 1998:
+    candidates_stmt = "SELECT * FROM candidate_master WHERE {0} LIKE '%%{1}%%';".format(field, query.upper())
+    names = pandas.read_sql(candidates_stmt, engine)
+    
+  
