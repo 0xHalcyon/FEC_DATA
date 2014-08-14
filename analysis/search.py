3a8cbe0f1fa29a9903b21dc030961cdd98722c82
Ferranti <support@planetcrypto.com>
2014-08-14 17:37:03 -0500

    Added database connection class

--- analysis/search.py
+++ analysis/search.py
@@ -8,43 +8,45 @@ from geo.geolocation import GeoLocation
 
 class SearchLocation:  
   'Provides search functions for database queries'
-  def __init__(self, conn_settings):
-    self.__geodb = "geozipcodes"
-    self.__db_prefix = conn_settings['db_prefix']
-    self.__db_user = conn_settings['db_user']
-    self.__db_password = conn_settings['db_password']
-    self.__db_host = conn_settings['db_host']
-    self.__db_port = conn_settings['db_port']
-    self.__year = conn_settings['year']
-    try:
-      self.__geo_conn = psycopg2.connect(dbname=self.__db_prefix+self.__geodb, \
-                                       user=self.__db_user,
-                                       password=self.__db_password,
-	                               host=self.__db_host,
-			               port=self.__db_port
-			               )
+  def __init__(self):
+    pass
+  #def __init__(self, conn_settings):
+    #self.__geodb = "geozipcodes"
+    #self.__db_prefix = conn_settings['db_prefix']
+    #self.__db_user = conn_settings['db_user']
+    #self.__db_password = conn_settings['db_password']
+    #self.__db_host = conn_settings['db_host']
+    #self.__db_port = conn_settings['db_port']
+    #self.__year = conn_settings['year']
+    #try:
+      #self.__geo_conn = psycopg2.connect(dbname=self.__db_prefix+self.__geodb, \
+                                       #user=self.__db_user,
+                                       #password=self.__db_password,
+	                               #host=self.__db_host,
+			               #port=self.__db_port
+			               #)
     
-      self.__geo_conn.set_client_encoding("UTF8")
-      self.__geo_cur = self.__geo_conn.cursor()
+      #self.__geo_conn.set_client_encoding("UTF8")
+      #self.__geo_cur = self.__geo_conn.cursor()
     
-      self.__fec_conn = psycopg2.connect(dbname=self.__db_prefix+str(self.__year), \
-                                       user=self.__db_user,
-                                       password=self.__db_password,
-	                               host=self.__db_host,
-			               port=self.__db_port
-			               )
-      self.__fec_conn.set_client_encoding("UTF8")
-      self.__fec_cur = self.__fec_conn.cursor()
-    except psycopg2.Error:
-      raise Exception("Did you define database parameters in config?")
+      #self.__fec_conn = psycopg2.connect(dbname=self.__db_prefix+str(self.__year), \
+                                       #user=self.__db_user,
+                                       #password=self.__db_password,
+	                               #host=self.__db_host,
+			               #port=self.__db_port
+			               #)
+      #self.__fec_conn.set_client_encoding("UTF8")
+      #self.__fec_cur = self.__fec_conn.cursor()
+    #except psycopg2.Error:
+      #raise Exception("Did you define database parameters in config?")
     
-  def __del__(self):
-    self.__fec_cur.close()
-    self.__geo_cur.close()
-    self.__fec_conn.close()
-    self.__geo_conn.close()
+  #def __del__(self):
+    #self.__fec_cur.close()
+    #self.__geo_cur.close()
+    #self.__fec_conn.close()
+    #self.__geo_conn.close()
     
-  def __get_candidate_committees__(self, cands):
+  def __get_candidate_committees__(self, Connect, cands):
     
     if self.__year <= 1998:
       cand_comms = {}
