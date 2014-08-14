e2de205c221891c8f9ede0d01e63e2d924ee1682
Ferranti <support@planetcrypto.com>
2014-08-14 02:21:13 -0500

    Add search by other function

--- analysis/search.py
+++ analysis/search.py
@@ -109,6 +109,7 @@ class SearchLocation:
       st = search_query.split(',')[1]
       query_stmt = "SELECT cand_name, cand_id, cand_pty_affiliation, cand_city, cand_st FROM candidate_master WHERE %s LIKE UPPER('%%%s%%') and %s LIKE UPPER('%%%s%%');" % \
 	            (city_key, city, st_key, st)
+      print query_stmt
     else:
       query_stmt = "SELECT DISTINCT cand_name, cand_id, cand_pty_affiliation, cand_city, cand_st FROM candidate_master WHERE %s LIKE UPPER('%%%s%%');" % (search_key, search_query)
     self.fec_cur.execute(query_stmt)
