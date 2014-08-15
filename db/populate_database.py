d65166ee73b75b82473c3170c894aa5258c97fc0
Ferranti <support@planetcrypto.com>
2014-08-15 02:20:20 -0500

    Add exception handling

--- db/populate_database.py
+++ db/populate_database.py
@@ -47,78 +47,48 @@ def populate_database(start_year, end_year, cwd, db_prefix, db_user, db_password
     except:
       print "Have you run 'make createdb' yet?"
       os.sys.exit(1)
-    if year <= 1998:
-     
-      for f in sorted(files_1998):
-        print "CURRENT YEAR: %s\nCURRENT TABLE: %s" % (year, f)
-        temp = open(files[f][0] % (cwd, year, year_suffix))
-        template = open(files[f][1] % cwd).read().strip()
-        template = str(tuple(template.split(","))).replace("'", "").lower()
-        for chunk in read_some_lines(temp):
-          for line in chunk:
-	    line.replace("\x92", "'")	    
-	    temp1 = line.strip().replace("'", "").split("|")
-	    if f in ("comm_to_comm", "cand_to_comm", "indiv_contrib"):
-	      if temp1[13]:
-	        date = temp1[13]
-	        try:
-	          date = datetime(month=int(date[0:2]), day=int(date[2:4]), year=int(date[4:]))
-	        except ValueError as e:
-		  print "Error: %s %s\nContinuing" % (e, temp1[13])
-		  to_write = "%s|%s\n" % (year, str(temp1))
-		  errors.write(to_write)
-		  errors.flush()
-		  continue
-	        temp1[13] = date.strftime("%Y%m%d")
-	      else:
-	        date = datetime(month=01, day=01, year=1900)
-	        temp1[13] = date.strftime("%Y%m%d")	   
-	    temp1 = tuple(temp1)
-	    try:
-	      query = "INSERT INTO %s %s VALUES %s;" % (f, template, temp1)
-              cur.execute(query)
-            except psycopg2.DataError as e:
-              print "Error: %s %s\nContinuing" % (e, temp1[13])
-              to_write = "%s|%s\n" % (year, str(temp1))
-	      errors.write(to_write)
-	      errors.flush()
-	      continue
-        conn.commit()
       
-    else:
-    
-      for f in sorted(files):
-        print "CURRENT YEAR: %s\nCURRENT TABLE: %s" % (year, f)
+    if year <= 1998:
+      files = files_1998
+      
+    for f in sorted(files):
+      print "CURRENT YEAR: %s\nCURRENT TABLE: %s" % (year, f)
+      try:
         temp = open(files[f][0] % (cwd, year, year_suffix))
         template = open(files[f][1] % cwd).read().strip()
-        template = str(tuple(template.split(","))).replace("'", "").lower()
-        for chunk in read_some_lines(temp):
-          for line in chunk:
-	    line.replace("\x92", "'")
-            temp1 = line.strip().replace("'", "").split("|")
-   	    if f in ("comm_to_comm", "cand_to_comm", "indiv_contrib"):
-	      if temp1[13]:
-	        date = temp1[13]
-	        try:
-	          date = datetime(month=int(date[0:2]), day=int(date[2:4]), year=int(date[4:]))
-	        except ValueError as e:
-		  print e, temp1[13]
-		  errors.write("%s|%s\n" % (year, str(temp1)))
-		  errors.flush()
-		  continue
-	        temp1[13] = date.strftime("%Y%m%d")
-	      else :
-	        date = datetime(month=01, day=01, year=1900)
-	        temp1[13] = date.strftime("%Y%m%d")	   
-	    temp1 = tuple(temp1)
-	    try:
-	      query = "INSERT INTO %s %s VALUES %s;" % (f, template, temp1)
-              cur.execute(query)
-            except psycopg2.DataError as e:
-              print "Error: %s %s\nContinuing" % (e, temp1[13])
-              to_write = "%s|%s\n" % (year, str(temp1))
-	      errors.write(to_write)
-	      errors.flush()
-	      continue
-          conn.commit()
+      except IOError:
+	print "Have you run 'make download && make extract yet'?"
+	os.sys.exit(1)
+      template = str(tuple(template.split(","))).replace("'", "").lower()
+      for chunk in read_some_lines(temp):
+        for line in chunk:
+	  line.replace("\x92", "'")	    
+	  temp1 = line.strip().replace("'", "").split("|")
+	  if f in ("comm_to_comm", "cand_to_comm", "indiv_contrib"):
+	    if temp1[13]:
+	      date = temp1[13]
+	      try:
+	        date = datetime(month=int(date[0:2]), day=int(date[2:4]), year=int(date[4:]))
+	      except ValueError as e:
+		print "Error: %s %s\nContinuing" % (e, temp1[13])
+		to_write = "%s|%s\n" % (year, str(temp1))
+		errors.write(to_write)
+		errors.flush()
+		continue
+	      temp1[13] = date.strftime("%Y%m%d")
+	    else:
+	      date = datetime(month=01, day=01, year=1900)
+	      temp1[13] = date.strftime("%Y%m%d")	   
+	  temp1 = tuple(temp1)
+	  try:
+	    query = "INSERT INTO %s %s VALUES %s;" % (f, template, temp1)
+            cur.execute(query)
+          except psycopg2.DataError as e:
+            print "Error: %s %s\nContinuing" % (e, temp1[13])
+            to_write = "%s|%s\n" % (year, str(temp1))
+	    errors.write(to_write)
+	    errors.flush()
+	    continue
+      conn.commit()
+      
     conn.close()
