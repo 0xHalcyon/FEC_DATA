eadde0283634a315bd2847475219be2c196f132b
Ferranti <support@planetcrypto.com>
2014-08-21 02:38:22 -0500

    Changed JSON dump output

--- analysis/search.py
+++ analysis/search.py
@@ -48,7 +48,7 @@ class SearchLocation:
         committee_ids = self.__Connection.cur.fetchall()
         for committee_id in committee_ids:
           cand_comms[candidate[0]]["comm_ids"].append(committee_id[0])
-    cand_comms = self.__remove_duplicates__(cand_comms)
+    #cand_comms = self.__remove_duplicates__(cand_comms)
     print cand_comms
     return cand_comms   
 	  
