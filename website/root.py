0a004f0063312c17f492bb811d499181d1e5167d
Ferranti <support@planetcrypto.com>
2014-08-20 19:13:34 -0500

    modified root.py

--- website/root.py
+++ website/root.py
@@ -15,14 +15,38 @@ class Root():
   def index(self):
     page = """<!DOCTYPE html>
 <html>
-<head>
-</head>
-<body>
-<iframe width="100%" height="100%" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=United%20States&key={0}"></iframe>
-</body>
+  <head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+    <style type="text/css">
+      html {{ height: 100% }}
+      body {{ height: 100%; margin: 0; padding: 0 }}
+      #map-canvas {{ height: 100% }}
+    </style>
+    <script type="text/javascript"
+      src="https://maps.googleapis.com/maps/api/js?key={0}">
+    </script>
+    <script type="text/javascript">
+      function initialize() {{
+        var mapOptions = {{
+          center: new google.maps.LatLng(39.50, -98.35),
+          zoom: 6
+        }};
+        var map = new google.maps.Map(document.getElementById("map-canvas"),
+            mapOptions);
+      }}
+      google.maps.event.addDomListener(window, 'load', initialize);
+    </script>
+  </head>
+  <body>
+    <div id="map-canvas"/>
+  </body>
 </html>
+
 """.format(self.__api_key)
     return page
+  def search(self, params):
+    pass
+
   if __name__ == '__main__':
     conn_settings = {'db_password': config.db_password, 
                  'db_user': config.db_user,
