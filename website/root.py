868fc4a19763d989754ceb8759cd0f46b79668d1
Ferranti <support@planetcrypto.com>
2014-08-20 20:55:42 -0500

    Messing around with google maps overlay

--- website/root.py
+++ website/root.py
@@ -21,32 +21,45 @@ class Root():
     <style type="text/css">
       html {{ height: 100% }}
       body {{ height: 100%; margin: 0; padding: 0; }}
-      .overlay {{ position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 10; background-color: rgba(0,0,0,0.5);}}
-      #map-canvas {{ height: 100%; margin: 0; position: relative}}
+      .overlay {{ position: absolute; top: 0; left: 0; width: 100%; height: 100%; z-index: 2; background-color: rgba(0,0,0,0.5);}}
+      #map-canvas {{ height: 100%; margin: 0; position: relative; z-index:1}}
     </style>
     <script type="text/javascript"
       src="https://maps.googleapis.com/maps/api/js?key={0}">
     </script>
     <script type="text/javascript">
       function initialize() {{
+        var positionMap = new google.maps.LatLng(39.50, -98.35);
+        
         var mapOptions = {{
-          center: new google.maps.LatLng(39.50, -98.35),
-          disableDefaultUI: true
+          center: positionMap,
+          disableDefaultUI: true,
           zoom: 4
         }};
+	
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
+            
+	var searchBoxString = '<div id="search-bar">' +
+                               '<form class="form-wrapper">' +
+                               '<input type="text" id="search" placeholder="Search by Name, City, State, or Zipcode ..." required>' +
+                               '<input type="submit" value="go" id="submit">' +
+                               '</form>' +
+                               '</div>';
+                               
+        var searchbox = new google.maps.InfoWindow({{content: searchBoxString}});
+        
+        var marker = new google.maps.Marker({{
+					      position: positionMap,
+					      map: map,
+					      title: 'FEC WATCHDOGS'
+					    }};
+	google.maps.event.addListener(marker, 'load', function() { searchbox.open(map, marker);});
       }}
       google.maps.event.addDomListener(window, 'load', initialize);
     </script>
   </head>
   <body>
-    <div id="search-bar" class="overlay">
-      <form class="form-wrapper">
-        <input type="text" id="search" placeholder="Search for CSS3, HTML5, jQuery ..." required>
-        <input type="submit" value="go" id="submit">
-      </form>
-    </div>
     <div id="map-canvas"/>
   </body>
 </html>
