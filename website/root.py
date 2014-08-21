ae5ef9262a1c5c61d11703333cc6330853dd2e77
Ferranti <support@planetcrypto.com>
2014-08-20 19:51:59 -0500

    Added search form, static css

--- website/root.py
+++ website/root.py
@@ -31,7 +31,7 @@ class Root():
       function initialize() {{
         var mapOptions = {{
           center: new google.maps.LatLng(39.50, -98.35),
-          zoom: 6
+          zoom: 4
         }};
         var map = new google.maps.Map(document.getElementById("map-canvas"),
             mapOptions);
