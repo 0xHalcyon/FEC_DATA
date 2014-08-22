0dad38dfd2b63cbac2633335befecd6cbac8e941
Ferranti <support@planetcrypto.com>
2014-08-22 00:49:04 -0500

    Added logo as marker

--- website/root.py
+++ website/root.py
@@ -53,7 +53,7 @@ class Root():
                                '</form>' +
                                '<form class="form-wrapper" action="searchByCityState" method="post"/>' +
                                '<input type="text" name="searchByCity" id="searchByCity" placeholder="...City/State (City not required)..."/>' +
-                               '<select name="searchByState" id="searchByState">' +
+                               '<select class="form-wrapper" name="searchByState" id="searchByState">' +
                                '       <option value="AL">Alabama</option>' +
                                '       <option value="AK">Alaska</option>' +
                                '       <option value="AZ">Arizona</option>' +
@@ -110,13 +110,13 @@ class Root():
                                '</form>' +
                                '<form class="form-wrapper" action="searchByZipcode" method="post"/>' +
                                '<input type="text" name="searchByZip" id="searchByZip" placeholder="...Or Zipcode" required/>' +
-                               '<select name="distanceRadius" id="searchDistance">' +
+                               '<select class="form-wrapper" name="distanceRadius" id="searchDistance">' +
                                '<option value="50">50</option>' +
                                '<option value="25">25</option>' +
                                '<option value="10">10</option>' +
                                '<option value="5">5</option>' +
                                '</select>' +
-                               '<select name="distanceUnit" id="searchUnit">' +
+                               '<select class="form-wrapper" name="distanceUnit" id="searchUnit">' +
                                '<option value="miles">Miles</option>' +
                                '<option value="kilometers">Kilometers</option>' +
                                '</select>' +
