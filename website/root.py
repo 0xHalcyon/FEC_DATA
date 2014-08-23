caeac70f3f2f7b9fda4bbb7e4ce2ea307806dfea
Ferranti <support@planetcrypto.com>
2014-08-23 02:38:05 -0500

    Removed google maps for time being

--- website/root.py
+++ website/root.py
@@ -33,125 +33,88 @@ class Root():
       html {{ height: 100% }}
       body {{ height: 100%; margin: 0; padding: 0; }}
       #map-canvas {{ height: 100%; margin: 0;}}
-    </style>
-    <script type="text/javascript"
-      src="https://maps.googleapis.com/maps/api/js?key={0}">
-    </script>
-    <script type="text/javascript">
-      function initialize() {{
-        var positionMap = new google.maps.LatLng(39.50, -98.35);
-        var positionMarker = new google.maps.LatLng(32, -98.35);
-        var mapOptions = {{
-          center: positionMap,
-          disableDefaultUI: true,
-          zoom: 5,
-          zoomControl: false,
-          scaleControl: false,
-          panControl: false,
-          overviewMapControl: false,
-          mapTypeControl: false
-        }};
-	
-        var map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
-            
-	var searchBoxString = '<div id="search-bar">' +
-                               '<form class="form-wrapper" action="searchByName" method="get"/>' +
-                               '<input type="text" name="searchByName" id="searchByName" placeholder="Search by Name..." required/>' +
-                               '<input type="submit" value="Go!" id="submitName"/>' +
-                               '</form>' +
-                               '<form class="form-wrapper" action="searchByCityState" method="get"/>' +
-                               '<input type="text" name="searchByCity" id="searchByCity" placeholder="...City/State (City not required)..."/>' +
-                               '<select name="searchByState" id="searchByState" required>' +
-                               '       <option value="AL" selected>Alabama</option>' +
-                               '       <option value="AK">Alaska</option>' +
-                               '       <option value="AZ">Arizona</option>' +
-                               '       <option value="AR">Arkansas</option>' +
-                               '       <option value="CA">California</option>' +
-                               '       <option value="CO">Colorado</option>' +
-                               '       <option value="CT">Connecticut</option>' +
-                               '       <option value="DE">Delaware</option>' +
-                               '       <option value="DC">District Of Columbia</option>' +
-                               '       <option value="FL">Florida</option>' +
-                               '       <option value="GA">Georgia</option>' +
-                               '       <option value="HI">Hawaii</option>' +
-                               '       <option value="ID">Idaho</option>' +
-                               '       <option value="IL">Illinois</option>' +
-                               '       <option value="IN">Indiana</option>' +
-                               '       <option value="IA">Iowa</option>' +
-                               '       <option value="KS">Kansas</option>' +
-                               '       <option value="KY">Kentucky</option>' +
-                               '       <option value="LA">Louisiana</option>' +
-                               '       <option value="ME">Maine</option>' +
-                               '       <option value="MD">Maryland</option>' +
-                               '       <option value="MA">Massachusetts</option>' +
-                               '       <option value="MI">Michigan</option>' +
-                               '       <option value="MN">Minnesota</option>' +
-                               '       <option value="MS">Mississippi</option>' +
-                               '       <option value="MO">Missouri</option>' +
-                               '       <option value="MT">Montana</option>' +
-                               '       <option value="NE">Nebraska</option>' +
-                               '       <option value="NV">Nevada</option>' +
-                               '       <option value="NH">New Hampshire</option>' +
-                               '       <option value="NJ">New Jersey</option>' +
-                               '       <option value="NM">New Mexico</option>' +
-                               '       <option value="NY">New York</option>' +
-                               '       <option value="NC">North Carolina</option>' +
-                               '       <option value="ND">North Dakota</option>' +
-                               '       <option value="OH">Ohio</option>' +
-                               '       <option value="OK">Oklahoma</option>' +
-                               '       <option value="OR">Oregon</option>' +
-                               '       <option value="PA">Pennsylvania</option>' +
-                               '       <option value="RI">Rhode Island</option>' +
-                               '       <option value="SC">South Carolina</option>' +
-                               '       <option value="SD">South Dakota</option>' +
-                               '       <option value="TN">Tennessee</option>' +
-                               '       <option value="TX">Texas</option>' +
-                               '       <option value="UT">Utah</option>' +
-                               '       <option value="VT">Vermont</option>' +
-                               '       <option value="VA">Virginia</option>' +
-                               '       <option value="WA">Washington</option>' +
-                               '       <option value="WV">West Virginia</option>' +
-                               '       <option value="WI">Wisconsin</option>' +
-                               '       <option value="WY">Wyoming</option>' +
-                               '</select>      ' +
-                               '<input type="submit" value="Go!" id="submitCity"/>' +
-                               '</form>' +
-                               '<form class="form-wrapper" action="searchByZipcode" method="get"/>' +
-                               '<input type="text" name="searchByZip" id="searchByZip" placeholder="...Or Zipcode" required/>' +
-                               '<select name="distanceRadius" id="searchDistance" required>' +
-                               '<option value="50" selected>50</option>' +
-                               '<option value="25">25</option>' +
-                               '<option value="10">10</option>' +
-                               '<option value="5">5</option>' +
-                               '</select>' +
-                               '<select name="distanceUnit" id="searchUnit" required>' +
-                               '<option value="miles" selected>Miles</option>' +
-                               '<option value="kilometers">Kilometers</option>' +
-                               '</select>' +
-                               '<input type="submit" value="Go!" id="submitZipcode"/>'+
-                               '</form>' +
-                               '</div>';
-                               
-        var searchbox = new google.maps.InfoWindow({{content: searchBoxString}});
-        
-        var marker = new google.maps.Marker({{
-					      position: positionMarker,
-					      map: map,
-					      title: 'FEC WATCHDOGS',
-					      icon: '/images/logo.png'
-					    }});
-	
-	google.maps.event.addListener(marker, 'click', function() {{ searchbox.open(map, marker);}});
-      }}
-      google.maps.event.addDomListener(window, 'load', initialize);
-    </script>
+    </style>            
   </head>
   <body>
-    <div id="map-canvas"/>
+    <div id="search-bar">
+      <form class="form-wrapper" action="searchByName" method="get"/>
+        <input type="text" name="searchByName" id="searchByName" placeholder="Search by Name..." required/>
+        <input type="submit" value="Go!" id="submitName"/>
+      </form>
+      <form class="form-wrapper" action="searchByCityState" method="get"/>
+        <input type="text" name="searchByCity" id="searchByCity" placeholder="...City/State (City not required)..."/>
+        <select name="searchByState" id="searchByState" required>
+          <option value="AL" selected>Alabama</option>
+          <option value="AK">Alaska</option>
+          <option value="AZ">Arizona</option>
+          <option value="AR">Arkansas</option>
+          <option value="CA">California</option>
+          <option value="CO">Colorado</option>
+          <option value="CT">Connecticut</option>
+          <option value="DE">Delaware</option>
+          <option value="DC">District Of Columbia</option>
+          <option value="FL">Florida</option>
+          <option value="GA">Georgia</option>
+          <option value="HI">Hawaii</option>
+          <option value="ID">Idaho</option>
+          <option value="IL">Illinois</option>
+          <option value="IN">Indiana</option>
+          <option value="IA">Iowa</option>
+          <option value="KS">Kansas</option>
+          <option value="KY">Kentucky</option>
+          <option value="LA">Louisiana</option>
+          <option value="ME">Maine</option>
+          <option value="MD">Maryland</option>
+          <option value="MA">Massachusetts</option>
+          <option value="MI">Michigan</option>
+          <option value="MN">Minnesota</option>
+          <option value="MS">Mississippi</option>
+          <option value="MO">Missouri</option>
+          <option value="MT">Montana</option>
+          <option value="NE">Nebraska</option>
+          <option value="NV">Nevada</option>
+          <option value="NH">New Hampshire</option>
+          <option value="NJ">New Jersey</option>
+          <option value="NM">New Mexico</option>
+          <option value="NY">New York</option>
+          <option value="NC">North Carolina</option>
+          <option value="ND">North Dakota</option>
+          <option value="OH">Ohio</option>
+          <option value="OK">Oklahoma</option>
+          <option value="OR">Oregon</option>
+          <option value="PA">Pennsylvania</option>
+          <option value="RI">Rhode Island</option>
+          <option value="SC">South Carolina</option>
+          <option value="SD">South Dakota</option>
+          <option value="TN">Tennessee</option>
+          <option value="TX">Texas</option>
+          <option value="UT">Utah</option>
+          <option value="VT">Vermont</option>
+          <option value="VA">Virginia</option>
+          <option value="WA">Washington</option>
+          <option value="WV">West Virginia</option>
+          <option value="WI">Wisconsin</option>
+          <option value="WY">Wyoming</option>
+        </select>      
+        <input type="submit" value="Go!" id="submitCity"/>
+      </form>
+      <form class="form-wrapper" action="searchByZipcode" method="get"/>
+        <input type="text" name="searchByZip" id="searchByZip" placeholder="...Or Zipcode" required/>
+          <select name="distanceRadius" id="searchDistance" required>
+            <option value="50" selected>50</option>
+            <option value="25">25</option>
+            <option value="10">10</option>
+            <option value="5">5</option>
+          </select>
+          <select name="distanceUnit" id="searchUnit" required>
+            <option value="miles" selected>Miles</option>
+            <option value="kilometers">Kilometers</option>
+          </select>
+        <input type="submit" value="Go!" id="submitZipcode"/>'+
+      </form>
+    </div>
   </body>
-</html>
-
-""".format(self.__api_key)
+</html>"""
     return page
   
   @cherrypy.expose
