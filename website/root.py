4dc12c0cc9f0946900d4d1c8d1743709f55fb587
Ferranti <support@planetcrypto.com>
2014-08-23 03:19:53 -0500

    Change CSS styling

--- website/root.py
+++ website/root.py
@@ -29,9 +29,9 @@ class Root():
   <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
     <link rel="stylesheet" href="/css/form.css">
+    <link rel="stylesheet" href="/css/style.css">
     <style type="text/css">
       html {{ height: 100% }}
-      body {{ height: 100%; margin: 0; padding: 0; background-image: url("/images/bkg.png");}}
       #map-canvas {{ height: 100%; margin: 0;}}
     </style>            
   </head>
